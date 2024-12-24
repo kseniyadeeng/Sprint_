8ce08cdd86cf81f20feaca66fa440fc665aaede5
Kseniya Kovalenko <kkovalenko051@gmail.com>
2024-12-24 19:54:04 +0300

    Changes

---
+++ helpers.py
@@ -0,0 +1,51 @@
+import requests
+import random
+import string
+from test_data import CREATE_COURIER, LOGIN_COURIER
+
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    random_string = ''.join(random.choice(letters) for _ in range(length))
+    return random_string
+
+
+def login_and_get_courier_id(payload):
+    login_response = requests.post(LOGIN_COURIER, json=payload)
+    assert login_response.status_code == 200
+    courier_id = login_response.json().get('id')
+    assert courier_id is not None
+    return courier_id
+
+
+def delete_courier(courier_id):
+    delete_response = requests.delete(f"{CREATE_COURIER}/{courier_id}")
+    assert delete_response.status_code == 200
+
+
+def register_new_courier_and_return_login_password():
+    def generate_random_string_for_login(length):
+        letters = string.ascii_lowercase
+        random_string = ''.join(random.choice(letters) for _ in range(length))
+        return random_string
+
+    login_pass = []
+
+    login = generate_random_string_for_login(10)
+    password = generate_random_string_for_login(10)
+    first_name = generate_random_string_for_login(10)
+
+    payload = {
+        "login": login,
+        "password": password,
+        "firstName": first_name
+    }
+
+    response = requests.post(CREATE_COURIER, data=payload)
+
+    if response.status_code == 201:
+        login_pass.append(login)
+        login_pass.append(password)
+        login_pass.append(first_name)
+
+    return login_pass
---
+++ test_data.py
@@ -0,0 +1,61 @@
+CREATE_COURIER = 'http://qa-scooter.praktikum-services.ru/api/v1/courier'
+LOGIN_COURIER = 'http://qa-scooter.praktikum-services.ru/api/v1/courier/login'
+CREATE_ORDER = 'http://qa-scooter.praktikum-services.ru/api/v1/orders'
+GET_ORDERS = 'http://qa-scooter.praktikum-services.ru/api/v1/orders'
+
+login_valid = 'harry_potter'
+password_valid = '111111!'
+
+order_data_grey = {
+    "firstName": "Harry",
+    "lastName": "Potter",
+    "address": "Hogwarts",
+    "metroStation": 4,
+    "phone": "+7 905 555 75 65",
+    "rentTime": 5,
+    "deliveryDate": "2024-12-24",
+    "comment": "Patronum",
+    "color": [
+        "GREY"
+    ]
+}
+
+order_data_black = {
+    "firstName": "Ron",
+    "lastName": "Weasley",
+    "address": "Hogwarts",
+    "metroStation": 3,
+    "phone": "+7 905 556 75 65",
+    "rentTime": 5,
+    "deliveryDate": "2024-12-24",
+    "comment": "No",
+    "color": [
+        "GREY"
+    ]
+}
+
+order_data_multicolor = {
+    "firstName": "Hermione",
+    "lastName": "Granger",
+    "address": "Hogwarts",
+    "metroStation": 3,
+    "phone": "+7 905 555 75 68",
+    "rentTime": 1,
+    "deliveryDate": "2024-12-24",
+    "comment": "I know everything",
+    "color": [
+        "GREY", "BLACK"
+    ]
+}
+
+order_data_no_color = {
+    "firstName": "Tom",
+    "lastName": "Riddle",
+    "address": "Azkaban",
+    "metroStation": 3,
+    "phone": "+7 905 555 09 88",
+    "rentTime": 4,
+    "deliveryDate": "2024-12-24",
+    "comment": "Harry Potter",
+    "color": []
+}
